e5b9272e28a3bad6725c686837dbedf56f4df7e2
Arco Dielhof <arcodielhof@hotmail.com>
2021-03-14 11:29:39 +0100

    Version 5 Release

--- main.lua
+++ main.lua
@@ -1,53 +1,29 @@
-local GlobalAddonName, AGU = ...
+local GlobalAddonName, ValorToolTips = ...
 
-local initialConfig = AGU.initialConfig
+local ValorToolTipsVersion = 5
 
-local AZPGUToolTipsVersion = 3
-local dash = " - "
-local name = "GameUtility" .. dash .. "ToolTips"
-local nameFull = ("AzerPUG " .. name)
-local promo = (nameFull .. dash ..  AZPGUToolTipsVersion)
+function ValorToolTips:OnLoad()
+    local clipAfter = string.find(ITEM_UPGRADE_TOOLTIP_FORMAT, "%%d") -1
+    local searchValue = string.sub(ITEM_UPGRADE_TOOLTIP_FORMAT, 1, clipAfter)
 
-local addonMain = LibStub("AceAddon-3.0"):NewAddon("GameUtility-ToolTips", "AceConsole-3.0")
-
-
-function AZP.GU.VersionControl:ToolTips()
-    return AZPGUToolTipsVersion
-end
-
-function AZP.GU.OnLoad:ToolTips()
-    print("Hoooi")
-
-    -- local oldOnShow = GameTooltip:GetScript("OnShow")
     GameTooltip:HookScript("OnTooltipSetItem", function (...) 
-        -- oldOnShow(...)
-
         local ttname = GameTooltip:GetName()
         for i = 1, GameTooltip:NumLines() do
             local left = _G[ttname .. "TextLeft" .. i]
             local text = left:GetText()
-            local cur, max =  text:match("Upgrade Level: (%d+)/(%d+)")
+            local cur, max = text:match(searchValue .. "(%d+)/(%d+)")
             if cur ~= nil then
-                -- cur = 5
                 if cur ~= max then
                     local _, itemLink = GameTooltip:GetItem()
                     local itemEquipLocation = C_Item.GetItemInventoryTypeByID(itemLink)
-                    local price = AGU.ValorData.ItemEquipLocation[itemEquipLocation]
+                    local price = ValorToolTips.ValorData.ItemEquipLocation[itemEquipLocation]
                     local levelsToMax = max - cur
                     local priceToMax = levelsToMax * price
-                    left:SetText(text .. string.format(" |cFF00FFFF%d/%d|r", price, priceToMax))
+                    left:SetText(text .. string.format("  |cFF00FFFF%d/%d|r", price, priceToMax))
                 end
             end
         end
     end)
-    -- ModuleStats["Frames"]["ToolTips"]:SetSize(250, 200)
-    -- GameTooltip:SetOwner(UIParent, "ANCHOR_NONE")
-    -- GameTooltip:SetInventoryItem("player", 5)
-    -- GameTooltip:Show()
-    
-
 end
 
-function AZP.GU.OnEvent:ToolTips(self, event, ...)
-
-end
+ValorToolTips:OnLoad()
