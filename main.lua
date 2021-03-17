13bf8605ec4edbee6107f9e7dbb037c0edc493d0
Arco Dielhof <arcodielhof@hotmail.com>
2021-03-17 14:14:57 +0100

    Added renaming frame to notify users of soon to be renamed. First auto upload test.

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local GlobalAddonName, ValorToolTips = ...
 
-local ValorToolTipsVersion = 8
+local ValorToolTipsVersion = 9
 local EventFrame, UpdateFrame = nil, nil
 local HaveShowedUpdateNotification = false
 
@@ -74,6 +74,54 @@ function ValorToolTips:OnLoad()
             end
         end
     end)
+
+    DelayedExecution(10,
+    function()
+        if AZPPopUpMessageShown ~= true then
+            local RenameFrame = CreateFrame("Frame", nil, UIParent, "BackdropTemplate")
+            RenameFrame:SetPoint("CENTER", 0, 250)
+            RenameFrame:SetSize(500, 200)
+            RenameFrame:SetBackdrop({
+                bgFile = "Interface/Tooltips/UI-Tooltip-Background",
+                edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
+                edgeSize = 12,
+                insets = { left = 1, right = 1, top = 1, bottom = 1 },
+            })
+            RenameFrame:SetBackdropColor(0.25, 0.25, 0.25, 0.80)
+            
+            RenameFrame.header = RenameFrame:CreateFontString("RenameFrame", "ARTWORK", "GameFontNormalHuge")
+            RenameFrame.header:SetPoint("TOP", 0, -10)
+            RenameFrame.header:SetText("|cFFFF0000AzerPUG's Valor ToolTips is getting renamed!|r")
+
+            RenameFrame.text = RenameFrame:CreateFontString("RenameFrame", "ARTWORK", "GameFontNormalLarge")
+            RenameFrame.text:SetPoint("TOP", 0, -40)
+            RenameFrame.text:SetText(
+                "Due to popularity and and requests, this addon will be renamed soon!\n\n" ..
+                "Instead of only focusing on Valor upgardable items,\n" ..
+                "it will also work for Conquest, Honor and Anima items!\n" ..
+                "(Did you noticed a 'Coming Soon!' message?\n\n" ..
+                "AzerPUG's ToolTips will be the new name of the addon,\n" .. 
+                "and slowly we will start adding more and more and more ToolTip features..."
+            )
+            local RenameFrameCloseButton = CreateFrame("Button", nil, RenameFrame, "UIPanelCloseButton")
+            RenameFrameCloseButton:SetWidth(25)
+            RenameFrameCloseButton:SetHeight(25)
+            RenameFrameCloseButton:SetPoint("TOPRIGHT", RenameFrame, "TOPRIGHT", 2, 2)
+            RenameFrameCloseButton:SetScript("OnClick", function() RenameFrame:Hide() end )
+
+            local NoShowAgainButton = CreateFrame("Button", nil, RenameFrame, "UIPanelButtonTemplate")
+            NoShowAgainButton:SetWidth(200)
+            NoShowAgainButton:SetHeight(25)
+            NoShowAgainButton:SetPoint("CENTER", 0, -80)
+            NoShowAgainButton:SetScript("OnClick", function() AZPPopUpMessageShown = true RenameFrame:Hide() end )
+            NoShowAgainButton.text = NoShowAgainButton:CreateFontString(nil, "ARTWORK", "GameFontNormalSmall")
+            NoShowAgainButton.text:SetText("Close and do not show again!")
+            NoShowAgainButton.text:SetWidth("200")
+            NoShowAgainButton.text:SetHeight("15")
+            NoShowAgainButton.text:SetPoint("CENTER", 0, -1)
+        end
+    end)
+    
 end
 
 function DelayedExecution(delayTime, delayedFunction)
